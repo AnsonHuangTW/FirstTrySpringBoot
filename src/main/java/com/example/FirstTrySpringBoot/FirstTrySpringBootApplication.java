039bcf0d560b360ad5d7acc311769559a5438b9e
hyhuang_win11Desktop(SourceTree) <anson10102@gmail.com>
2024-12-10 14:27:42 +0800

    init SB project from https://start.spring.io

---
+++ src/main/java/com/example/FirstTrySpringBoot/FirstTrySpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.FirstTrySpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstTrySpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstTrySpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FirstTrySpringBoot/FirstTrySpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FirstTrySpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstTrySpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
