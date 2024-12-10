2156a00834bc7538f29c8b8980ba83b2206d1a20
hyhuang_win11Desktop(SourceTree) <anson10102@gmail.com>
2024-12-10 19:59:52 +0800

    @PostConstruct for init var in bean

--- src/main/java/com/example/FirstTrySpringBoot/Controller/HpPrinter.java
+++ src/main/java/com/example/FirstTrySpringBoot/Controller/HpPrinter.java
@@ -1,12 +1,23 @@
 package com.example.FirstTrySpringBoot.Controller;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Component;
 
 @Component
 public class HpPrinter implements Printer{
 
+    private int count;
+
+    @PostConstruct
+    public void init(){
+//        init the count for bean
+        count = 10;
+    }
+
     @Override
     public void print(String messager) {
+        count--;
         System.out.println("HP Printer: " + messager);
+        System.out.println("Limit to print: " + count);
     }
 }
