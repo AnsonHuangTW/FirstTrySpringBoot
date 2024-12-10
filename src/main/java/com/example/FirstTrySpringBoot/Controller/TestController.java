57c7052efa71fe298232b4641ebb8f38aec56870
hyhuang_win11Desktop(SourceTree) <anson10102@gmail.com>
2024-12-10 19:52:02 +0800

    @Autowired + @Qualifier while have mutilpe beans (classes implements same interface)

--- src/main/java/com/example/FirstTrySpringBoot/Controller/TestController.java
+++ src/main/java/com/example/FirstTrySpringBoot/Controller/TestController.java
@@ -1,6 +1,7 @@
 package com.example.FirstTrySpringBoot.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     @Autowired
+    @Qualifier("hpPrinter")
     private Printer printer;
+
+//    @Autowired
+//    @Qualifier("canonPrinter")
+//    private Printer printer;
 
     @RequestMapping("/test-page")
     public String TestForShowText(){
